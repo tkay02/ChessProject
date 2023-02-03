2d9d8a9dc4e1c4ee188e47eb199e9237d8734796
nolan <nolan@152.30.201.160>
2023-02-02 22:49:51 -0500

    Created NolanFlinchum.txt and fixed typos

--- Driver.java
+++ Driver.java
@@ -1,13 +1,7 @@
-public class Info {
+public class Driver {
 	
 	public static void main(String[] args) {
-		
-		System.out.println("Name: " + "\t" + "Thomas Kay" + 
-						   "\nDate: " + "\t" + "2/2/2023" +
-						   "\nTeam Color: " + "\t" + "Red" +
-						   "\nTeam Name: " + "\t" + "The A Team" +
-						   "\nEmail: " + "\t" + "takay1@catamount.wcu.edu");
-		
+		System.out.println("CS364 Team Red First Commit");
 	}
 	
 }
