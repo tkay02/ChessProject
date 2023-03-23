2d635c601686d429dab0728a53f163045fb6d48d
jaoladeji <joladeji03@gmail.com>
2023-03-23 11:46:08 -0400

    Added ANSI Color and documentation

--- src/enums/File.java
+++ src/enums/File.java
@@ -1,12 +1,19 @@
 package src.enums;
 
 /**
+ * This enumeration class models the File also known as the column for the 
+ * chess board. The letters representing the possible selections for the 
+ * columns and they have the realFile, which is the column letter that will
+ * be displayed to the user and the array file, which is the corresponding
+ * index within the array.
+ * 
  * @author Joseph Oladeji
  * @author Thomas Kay
  * @author Levi Sweat
  * @author Nolan Flinchum
  * @version
  */
+
 public enum File {
     A("A", 0),  B("B", 1),  
     C("C", 2),  D("D", 3),  
