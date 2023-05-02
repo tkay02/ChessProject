4d488cf92eb8901950599decdaba0c0085aa352e
Levi-Sweat <lsweat922@gmail.com>
2023-05-01 20:34:14 -0400

    Initial progress on SettingsGUI

---
+++ src/ui_gui/SettingsGUI.java
@@ -0,0 +1,92 @@
+package src.ui_gui;
+
+import java.util.Arrays;
+
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class SettingsGUI extends VBox{
+
+	/**The current stage for the application**/
+	Stage stage;
+	
+	/**the root scene for this app**/
+	Scene rootScene;
+	
+	/**The button options of the menu**/
+	Button b1, b2, b3, b4, b5, b6, b7, b8;
+	
+	/**Construct the menu**/
+	public SettingsGUI(){
+		
+		this.setAlignment(Pos.CENTER);//SET the alignment of the  layout.
+		this.setSpacing(20);//Spacing between components.
+		this.setId("Screen0");//The CSS id for this screen
+		
+		//Menu Title
+		Label lbl = new Label("Settings");
+		lbl.getStyleClass().add("screenTitle");
+		this.getChildren().add(lbl);
+
+		//colors label
+		Label lb2 = new Label("Colors:");
+		lb2.getStyleClass().add("screenTitle");
+		//this.getChildren().add(lb2);
+		
+		VBox v1 = new VBox();
+		this.getChildren().add(v1);
+		v1.getChildren().add(lb2);
+		
+		
+		CheckBox cb1 = new CheckBox("Show Moves");
+		VBox v2 = new VBox();
+		this.getChildren().add(v2);
+		v2.getChildren().add(cb1);
+		
+		
+//		//menu option1
+//		b1 = new Button("Play Chess");  
+//		b1.getStyleClass().add("buttonStyleA");
+//		b1.getStyleClass().add("buttonSizeS");
+//	    
+//	    //Menu option2
+//		b2 = new Button("View Rules");  
+//		b2.getStyleClass().add("buttonStyleA");
+//		b2.getStyleClass().add("buttonSizeS");
+//	    
+//	    //Menu option3
+//	    b3 = new Button("Sign In");  
+//		b3.getStyleClass().add("buttonStyleA");
+//		b3.getStyleClass().add("buttonSizeS");
+//	    
+//	    b4 = new Button("Sign Up");  
+//	    b4.getStyleClass().add("buttonStyleA");
+//	    b4.getStyleClass().add("buttonSizeS");
+//	    
+//	    b5 = new Button("Define Players");  
+//	    b5.getStyleClass().add("buttonStyleA");
+//	    b5.getStyleClass().add("buttonSizeS");
+//	    
+//	    
+//	    b6 = new Button("Settings");  
+//	    b6.getStyleClass().add("buttonStyleA");
+//	    b6.getStyleClass().add("buttonSizeS");
+//	    
+//	    b7 = new Button("Load Game");  
+//	    b7.getStyleClass().add("buttonStyleA");
+//	    b7.getStyleClass().add("buttonSizeS");
+//	    
+//	    b8 = new Button("Quit");  
+//	    b8.getStyleClass().add("buttonStyleA");
+//	    b8.getStyleClass().add("buttonSizeS");
+//	    this.getChildren().addAll(Arrays.asList(b1, b2, b3, b4, b5, b6, b7, b8));
+	}
+	
+	
+}
